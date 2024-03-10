b9f3285fe579392b89f107f7667293d07f040368
zaadevofc <93970726+zaadevofc@users.noreply.github.com>
2024-03-10 07:34:31 +0700

    .

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { PrismaClient } = require("@prisma/client");
 const express = require("express");
 const app = express();
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 app.get("/", (req, res) => {
   res.send({
